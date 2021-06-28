9ed1c12be3c7fe395e89497302e27d5379e0bfba
Bartłomiej Komendarczuk <bkomendarczuk@gmail.com>
2021-06-28 18:02:37 +0200

    Init project

---
+++ Compilation/CompilationSync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OptimaSync.Confiugration;
+
+namespace OptimaSync.Compilation
+{
+    public class CompilationSync
+    {
+        PathSetup pathSetup = new PathSetup();
+        public void DownloadLastCompilation(String compilationSourcePath)
+        {
+          compilationSourcePath = pathSetup.GetCompilationSourcePath;
+
+            if (string.IsNullOrEmpty(compilationSourcePath){
+                throw new NullReferenceException("Ustaw lokalizację z której chcesz pobierać pliki kompilacji");
+            }
+
+        }
+    }
+}
---
+++ Configuration/PathSetup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimaSync.Confiugration
+{
+    public class PathSetup
+    {
+        private string compilationSourcePath { get; set; }
+        private string compilationDestPath { get; set; }
+        private string compilationSOAPath { get; set; }
+
+        public string GetCompilationSourcePath => compilationSourcePath;
+        public string GetCompilationDestPath => compilationDestPath;
+        public string GetCompilationSOAPath => compilationSOAPath;
+
+
+    }
+}
