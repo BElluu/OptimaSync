eaa6a11f068992acd8b21f5142c84b5e2411c212
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2021-10-08 11:01:46 +0200

    Application is single instance

---
+++ ApplicationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptimaSync
+{
+    public class ApplicationController: WindowsFormsApplicationBase
+    {
+        private MainForm mainForm;
+
+        public ApplicationController(MainForm form)
+        {
+            mainForm = form;
+            this.IsSingleInstance = true;
+            this.StartupNextInstance += StartNextInstance;
+        }
+
+        private void StartNextInstance(object sender, StartupNextInstanceEventArgs e)
+        {
+            e.BringToForeground = true;
+            mainForm.showUpTrayWhenNewInstance();
+        }
+
+        protected override void OnCreateMainForm()
+        {
+            this.MainForm = mainForm;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace OptimaSync
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            var controller = new ApplicationController(new MainForm());
+            controller.Run(Environment.GetCommandLineArgs());
         }
 
         private static void ConfigureSerilog()
