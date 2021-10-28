b9eff264a9bf0788c48c3ff04736ea52e65f2b08
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2021-10-28 11:13:30 +0200

    Prepare to change to appsetting file.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+using System.Collections.Specialized;
 using OptimaSync.Helper;
 using OptimaSync.Service;
 using OptimaSync.UI;
@@ -13,6 +15,16 @@ namespace OptimaSync
         [STAThread]
         static void Main()
         {
+            string downloadTypeSetting = ConfigurationManager.AppSettings.Get("DownloadType");
+            bool runOptimaSetting = Convert.ToBoolean(ConfigurationManager.AppSettings.Get("RunOptima"));
+            string latestVersionCheckedSetting = ConfigurationManager.AppSettings.Get("LatestVersionChecked");
+            string compilationPathSetting = ConfigurationManager.AppSettings.Get("CompilationPath");
+            string destinationSetting = ConfigurationManager.AppSettings.Get("Destination");
+            string soaDestinationSetting = ConfigurationManager.AppSettings.Get("SOADestination");
+            string programmerDestinationSetting = ConfigurationManager.AppSettings.Get("ProgrammerDestination");
+            bool autoCheckVersion = Convert.ToBoolean(ConfigurationManager.AppSettings.Get("AutoCheckVersion"));
+            bool notificationSound = Convert.ToBoolean(ConfigurationManager.AppSettings.Get("NotificationSound"));
+
             ConfigureSerilog();
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
